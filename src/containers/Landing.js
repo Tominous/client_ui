858a8692f3cc369cbc825d4fbeaa58840ef4ce86
Admin <admin@Admins-Mac.local>
2019-06-21 05:19:27 +0300

    added onload to landing page, added integrated with create contract api

--- src/containers/Landing.js
+++ src/containers/Landing.js
@@ -1,9 +1,21 @@
 import React, {Component} from 'react';
-
+import Services from '../services/services';
 
 class Landing extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      isNew: false,
+      accounts: []
+    };
+  }
   componentDidMount() {
+    let that = this;
+    Services.onload()
+    .then(function(res){
+      that.setState({accounts: res.accounts});
+    }) 
     for (let index = 0; index < document.getElementsByClassName('menu-button').length; index++) {
       document.getElementsByClassName('menu-button')[index].classList.remove('active');
     }
